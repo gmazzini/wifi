099e97dc68764db734bcd47db0bbeeea673b565e
Gianluca Mazzini <gianluca@mazzini.org>
2022-01-22 06:11:11 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-$ipaddr="";
-if(getenv("HTTP_CLIENT_IP"))$ipaddr=getenv("HTTP_CLIENT_IP");
-else if(getenv("HTTP_X_FORWARDED_FOR"))$ipaddr=getenv("HTTP_X_FORWARDED_FOR");
-else if(getenv("HTTP_X_FORWARDED"))$ipaddr=getenv("HTTP_X_FORWARDED");
-else if(getenv("HTTP_FORWARDED_FOR"))$ipaddr=getenv("HTTP_FORWARDED_FOR");
-else if(getenv("HTTP_FORWARDED"))$ipaddr=getenv("HTTP_FORWARDED");
-else if(getenv("REMOTE_ADDR"))$ipaddr=getenv("REMOTE_ADDR");
-else $ipaddr="UNKNOWN";
+
+include "utility.php";
 $ii=explode(".",$ipaddr);
 ?>
 
