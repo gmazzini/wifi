0b8112390c313f82b81dea589340ad108af125dc
Gianluca Mazzini <gianluca@mazzini.org>
2022-01-24 06:33:09 +0100

    Update run.php

--- run.php
+++ run.php
@@ -17,14 +17,9 @@ $trem=1000-($actt-$mytime);
 if($trem<0){echo "TEMPO SCADUTO IDENTIFICAZIONE NON AVVENUTA, PUOI CHIUDERE LA PAGINA"; exit(); }
 echo "SECONDI RIMASTI PER IDENTIFICARSI $trem";
 
-echo "http://44.134.207.253:8888/gmrecv.php?token=$mytoken&from=$cell";
 $page=file("http://44.134.207.253:8888/gmrecv.php?token=$mytoken&from=$cell");
-print_r($page);
 $dd=explode("|",end($page));
 if(substr($dd[0],0,6)==$randval && $sendto==trim($dd[1])){
-  echo "hhhh";
-  
-
   $mac=maclogip($ipaddr);
   $mys=mysqli_connect("localhost",$sqluser,$sqlpassword,"wifi");
   mysqli_query($mys,"INSERT INTO users (timestamp,cell,mac,valid) VALUES ($mytime,'$cell','$mac',1)");
