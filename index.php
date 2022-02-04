aa1ae9bffffdee1542cfbc3ba55efb49655bc57b
Gianluca Mazzini <gianluca@mazzini.org>
2022-02-04 06:59:43 +0100

    Update index.php

--- index.php
+++ index.php
@@ -26,6 +26,6 @@ Inserisci qui il tuo numero di cellulare<br>
 Nota che il cellulare ti identifica e deve necessariamente<br>
 essere tuo personale<br>
 Se prosegui stai anche prendendo atto della informativa sul servizio<br>
-e sulla privacy qui riportata xxxx e la stai accettando
+e sulla privacy qui riportata xxxx e la stai accettando<br>
 <img src="logo.png" alt="logo" width="300" height="auto">  
 </html>
